3201e24e0b66895e4985780d7cee84c1b5ae966d
Ahana Garadi <agaradi@clevelandheartlab.com>
2019-10-27 23:26:49 -0400

    Unit tests

---
+++ MasterMind.UnitTests/MasterMindProjTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MasterMind;
+
+namespace MasterMind.UnitTests
+{
+    [TestClass]
+    public class MasterMindProjTests
+    {
+        MasterMindProj proj;
+
+        [TestInitialize]
+        public void before()
+        {
+            proj = new MasterMindProj();
+        }
+
+        [TestMethod]
+        public void generateSecretNumber_returnTrue()
+        {
+            string result = proj.generateSecretNumber();
+            Assert.IsTrue(result.Length == 4);
+        }
+
+        [TestMethod]
+        public void validateInput_4digits_returnTrue()
+        {
+            bool result = proj.validateInput("1234");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void validateInput_outOfRangeInput_returnFalse()
+        {
+            bool outOfRangeInt = proj.validateInput("7889");
+            bool outOfRangeChars = proj.validateInput("12c34");
+            Assert.IsFalse(outOfRangeInt);
+            Assert.IsFalse(outOfRangeChars);
+        }
+
+        [TestMethod]
+        public void validateInput_lessThan4digits_returnFalse()
+        {
+            bool result = proj.validateInput("124");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void validateInput_greaterThan4digits_returnFalse()
+        {
+            bool result = proj.validateInput("12425");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void compare_matchSecretAndGuess_returnTrue()
+        {
+            proj.freq = new System.Collections.Generic.List<string>(){ "1","2","4"};
+            char[] preResult = { '+', '+', '+', '+' };
+            char[] result = proj.compare("1224", "1224");
+            CollectionAssert.AreEqual(result, preResult);
+        }
+
+        [TestMethod]
+        public void compare_noMatchSecretAndGuess_returnFalse()
+        {
+            proj.freq = new System.Collections.Generic.List<string>() { "1", "2", "4" };
+            char[] preResult = { '+', '+', '+', '+' };
+            char[] result = proj.compare("1224", "1234");
+            CollectionAssert.AreNotEqual(result, preResult);
+        }
+    }
+}
---
+++ MasterMind.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MasterMind.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MasterMind.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a3f2daa2-f100-4b75-b887-7f9255493e32")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
