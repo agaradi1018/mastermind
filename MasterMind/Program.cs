9bce448d398031be4a046c08d02e657f3f832fa2
Ahana Garadi <agaradi@clevelandheartlab.com>
2019-10-27 23:33:01 -0400

    UnitTests Included

---
+++ MasterMind/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] answer;
+            string guess = "";
+            bool result;
+
+            MasterMindProj master = new MasterMindProj();
+            string secret = master.generateSecretNumber();
+
+            Console.WriteLine("Let's play Mastermind! - You have to guess a 4 digit secret number where each digit is beween 1 & 6.");
+            Console.WriteLine("'+' -> correct digit, correct position\n'-' -> correct digit, wrong position\n' '(blank) -> wrong number. You have 10 attempts, GOOD LUCK!");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                result = true;
+                Console.WriteLine("\nAttempt # " + i + ", Enter you guess - ");
+                guess = Console.ReadLine();
+                while (!master.validateInput(guess))
+                {
+                    Console.WriteLine("Please enter a 4 digit number. Each digit shoud be an integer between 1 and 6. ");
+                    guess = Console.ReadLine();
+                }
+
+                answer = master.compare(secret, guess);
+
+                foreach(char c in answer)
+                {
+                    if (c != '+')
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+
+                if (result)
+                {
+                    Console.WriteLine("You got the correct answer!! Congratulations, the secret number was " + secret + "!");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    Console.Write("Result : ");
+                    foreach (char c in answer)
+                        Console.Write(c + "|");
+                }
+            }
+            Console.WriteLine("You've run out of turns, the secret number was " + secret + ". Better luck next time..");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MasterMind/masterMindProj.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasterMind
+{
+    public class MasterMindProj
+    {
+        public List<string> freq;
+        
+        public MasterMindProj(){}
+         
+        public string generateSecretNumber()
+        {
+            freq = new List<string>();
+            Random r = new Random();
+            string num = "";
+            StringBuilder secret = new StringBuilder();
+
+            try
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    num = (r.Next(1, 7)).ToString();
+                    secret.Append(num);
+                    if (!freq.Contains(num))
+                        freq.Add(num);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new System.ArgumentException("Error in denerating sevret number : " + ex.Message);
+            }
+
+            return secret.ToString();
+        }
+
+        public bool validateInput(string guess)
+        {
+            try
+            {
+                if (!System.Text.RegularExpressions.Regex.IsMatch(guess, "^[1-6]{4}$"))
+                {
+                    return false;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new System.ArgumentException("Error in validating input : " + ex.Message);
+            }
+            return true;
+        }
+
+        public char[] compare(string secret, string guess)
+        {
+            string num;
+            int matchCounter = 0;
+            char[] answer = new char[4];
+
+            try
+            {
+                for (int i = 0; i < secret.Length; i++)
+                {
+                    num = guess[i].ToString();
+
+                    if (freq.Contains(num))
+                    {
+                        if (num == secret[i].ToString())
+                        {
+                            answer[i] = '+';
+                            matchCounter++;
+                        }
+                    }
+                }
+
+                if (matchCounter == 4)
+                {
+                    return answer;
+                }
+
+                for (int i = 0; i < secret.Length; i++)
+                {
+                    if (answer[i] != '+')
+                    {
+                        num = guess[i].ToString();
+
+                        if (freq.Contains(num))
+                        {
+                            answer[i] = '-';
+                        }
+                        else
+                        {
+                            answer[i] = ' ';
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new System.ArgumentException("Error in compare method : " + ex.Message);
+            }
+            return answer;
+        }
+    }
+}
